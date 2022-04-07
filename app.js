132fb97cf60425450c0cd1ac1f510337c45f89cf
farangpal <98533096+farangpal@users.noreply.github.com>
2022-04-07 11:50:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function sendMessage(cont) {
         // replace the url in the "open" method with yours
         request.setRequestHeader('Content-type', 'application/json');
         const params = {
-            username: "Sky",
+            username: "Imaginary Ones",
             avatar_url: "",
             content: cont
         }
